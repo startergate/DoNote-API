b356e3c6c3cfd2450b2add5c13935e90151108b4
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-04-25 18:57:48 +0900

    Add /api/getShared.php

--- getShared.php
+++ getShared.php
@@ -1,1 +1,17 @@
 <?php
+  require '../config/config.php';
+  header('Access-Control-Allow-Origin: *');
+  header('Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS');
+  header('Access-Control-Max-Age: 3600');
+  header('Access-Control-Allow-Headers: Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization');
+  header('Content-Type: application/json');
+  $conn = new mysqli($config['host'], $config['duser'], $config['dpw'], $config['dname']);  //Note Database
+  $sql = 'SELECT shareTable, shareID FROM sharedb_'.$_POST['pid'];
+  $result = $conn->query($sql);
+  $output = array('data' => []);
+  if ($result) {
+      while ($row = $result->fetch_assoc()) {
+          $output['data'][] = ['noteid' => $row['shareTable'], 'shareid' => $row['shareID']];
+      }
+  }
+  echo urldecode(json_encode($output));
